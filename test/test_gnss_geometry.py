d4ecc460ef653cef2ba063c1260c8bf034a3e4a4
maaijke <mevius@astron.nl>
2025-12-08 22:15:25 +0100

    add test data

--- test/test_gnss_geometry.py
+++ test/test_gnss_geometry.py
@@ -9,20 +9,20 @@ from astropy.coordinates import EarthLocation
 datapath = Path("./data/")
 
 def test_get_sat_pos_object():
-    sp3_files = [Path(i) for i in sorted(glob.glob(datapath.as_posix() +  "/*sp3.gz"))]
+    sp3_files = [Path(i) for i in sorted(glob.glob(datapath.as_posix() +  "/*SP3.gz"))]
     sat_pos_object = get_sat_pos_object(sp3_files = sp3_files)
     return sat_pos_object
 
 
 def test_get_sat_pos():
     sat_pos_object = test_get_sat_pos_object()
-    times  = Time("2020-12-17T08:05:00") + np.arange(4) * 13 * u.min
+    times  = Time("2025-06-26T12:05:00") + np.arange(4) * 13 * u.min
     sat_pos = get_sat_pos(obs=sat_pos_object, times=times, sat_name="G02")
     return sat_pos
 
 def test_get_stat_sat_ipp():
 
     satpos = test_get_sat_pos()
-    gnsspos= EarthLocation.from_geodetic(6 * u.deg, 42 *u.deg, 0 *u.m)
-    times = Time("2020-12-17T08:05:00") + np.arange(4) * 13 * u.min
+    gnsspos= EarthLocation.from_geodetic(6 * u.deg, 52 *u.deg, 0 *u.m)
+    times = Time("2025-06-26T12:05:00") + np.arange(4) * 13 * u.min
     return get_stat_sat_ipp(satpos=satpos, gnsspos=gnsspos, times=times)
