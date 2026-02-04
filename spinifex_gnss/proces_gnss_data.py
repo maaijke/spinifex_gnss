aab668db994c19b4ac4b2f40ba62d748ed827fd2
maaijke <mevius@astron.nl>
2026-02-04 20:31:02 +0100

    reuce dimension of par

--- spinifex_gnss/proces_gnss_data.py
+++ spinifex_gnss/proces_gnss_data.py
@@ -463,11 +463,9 @@ def get_interpolated_tec(
                 + (((INTERPOLATION_ORDER) ** 2 + INTERPOLATION_ORDER) // 2,),
                 dtype=float,
             )
-            print ("A",A.shape)
             weight = (
                 1.0 / vtec_dlong_dlat[dist_select][:, 1]
             )  # inverse variance weights
-            print ("weight",weight.shape)
             idx=0
             for ilon in range(0,INTERPOLATION_ORDER):
                 for ilat in range(0,INTERPOLATION_ORDER-ilon):
@@ -485,13 +483,12 @@ def get_interpolated_tec(
             w = w * np.eye(A.shape[0])
             AwT = A.T @ w
             try:
-                par = np.linalg.inv(AwT @ A) @ (
+                par = (np.linalg.inv(AwT @ A) @ (
                     AwT @ vtec_dlong_dlat[dist_select][:, :1]
-                )
+                )).squeeze()
             except:
                 print("inverse fail", AwT.shape, w)
                 continue
-            print ("par", par[0])
             fitted_density[timeidx, hidx] = par[0]  # We need the offset at the origin
     return fitted_density
 
